51ea0b641e6f489a0532862b2c45aabae3f9d0e7
Anshika Srivastava <67851072+Anshika-Srivastava@users.noreply.github.com>
2022-02-05 22:12:50 +0530

    Delete src directory

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-
-//Ye pura function app component ko root mein render kar rha hai
-//Ye pure project mein ek hi jagah likhenge
-ReactDOM.render(<App />, document.getElementById('root'));
